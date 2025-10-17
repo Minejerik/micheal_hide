5ac0607087faff8a2e07ecc405c5860dbb25af27
Minejerik <jaanerikf@gmail.com>
2025-10-16 21:41:03 -0400

    Working on bullets

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,1 +1,24 @@
 extends RigidBody2D
+
+
+
+func fire(facing:float, speed:float):
+	### Handles Firing of the bullet, needs rotation, in radians, and speed
+	print("Firing w "+str(facing)+" Facing, and "+str(speed))
+	rotate(facing)
+	apply_central_force(Vector2(facing, speed))
+	
+
+
+func _on_death_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_body_entered(_body: Node) -> void:
+	$Collision_particles.emitting = true
+	linear_velocity = Vector2.ZERO
+	$Sprite2D.hide()
+
+
+func _on_collision_particles_finished() -> void:
+	queue_free()
--- scripts/bullet_fire.gd
+++ scripts/bullet_fire.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 const bullet = preload("res://scenes/objects/bullet.tscn")
 
+var temp = 0.0
+
 func fire_bullet():
 	var bullet_real = bullet.instantiate()
+	bullet_real.fire(temp, 50)
+	temp += 3.14
 	add_child(bullet_real)
 	
 	
