11aa63e11ce4242b965eb97998b6c9323e10d0a8
Minejerik <jaanerikf@gmail.com>
2025-10-18 20:53:07 -0400

    A lot of stuff

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -10,42 +10,123 @@ var player_body = null
 # The bullet firer
 @onready var bulletfire = $BulletFire
 @onready var firetimer = $FireTimer
+@onready var nav_agent = $NavigationAgent2D
+
+# Used for pursuing, starts it, and is followed
+@export var target_names = [""]
+# Use for walking, the path it follows
+@export var path: Path2D
+# Wether the enemy is alerted to a target, such as entering colider
+@export var alerted: bool = false
+
+# WALKING -> Following the path set above
+# WANDERING -> Wandering around, no set path
+# PURSUING -> Target was found, pursue, until target lost
+
+enum State {WALKING, WANDERING, PURSUING}
+
+@export var current_state = State.WALKING
+@export var default_state = State.WALKING
+
+# The current node in the path that it should go towards when walking
+var WALKING_current_node = 0
+var WALKING_target_node = null
+
+var speed = 50
+
+func _ready():
+	WALKING_target_node = path.curve.get_point_position(0)
+	if default_state == State.WALKING:
+		init_walking()
 
 func _physics_process(_delta: float) -> void:
+	look_at(nav_agent.get_next_path_position())
+	if alerted:
+		player_in_sight = check_player_visible()
+	else:
+		current_state = default_state
+	
 	if player_in_sight:
-		if player_ray_cast_check:
-			var distance_to_player = global_position.distance_to(player_body.global_position)
-			look_at(player_body.position)
-			if distance_to_player < 100 && firetimer.is_stopped():
-				firetimer.start()
-		else:
-			var space_state = get_world_2d().direct_space_state
-			var query = PhysicsRayQueryParameters2D.create(global_position, player_body.global_position)
-			var result = space_state.intersect_ray(query)
-			if result:
-				if result.collider.name == "player":
-					player_ray_cast_check = true
-					
+		current_state = State.PURSUING
+	
+	match current_state:
+		State.WALKING:
+			handle_walking()
+		State.WANDERING:
+			pass
+		State.PURSUING:
+			handle_pursuing()
+			
+	move()
+
+func handle_walking():
+	look_at(nav_agent.get_next_path_position())
+
+func init_walking():
+	WALKING_current_node += 1
+	if WALKING_current_node >= path.curve.point_count:
+		WALKING_current_node = 0
+	WALKING_target_node = path.curve.get_point_position(WALKING_current_node)
+
+func move():
+	# All moving, using pathfinding
+	var direction = nav_agent.get_next_path_position() - global_position
+	direction = direction.normalized()
+	velocity = direction * speed
+	nav_agent.set_velocity(velocity)
+		
+	#move_and_slide()
+	
+func _target_reached_nav() -> void:
+	if current_state == State.WALKING:
+		init_walking()
 
 
+func handle_pursuing():
+	var distance_to_player = global_position.distance_to(player_body.global_position)
+	if distance_to_player < 100 && firetimer.is_stopped():
+		firetimer.start()
+		
+	
+func check_player_visible():
+	# Checks if the player is not blocked by anything, using a raycast
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(global_position, player_body.global_position)
+	var result = space_state.intersect_ray(query)
+	if result:
+		if result.collider == player_body:
+			nav_agent.target_position = player_body.global_position
+			return true
+	return false
+
+func _on_player_find_timeout_timeout() -> void:
+	if current_state == State.PURSUING:
+		nav_agent.target_position = player_body.global_position
+	elif current_state == State.WALKING:
+		nav_agent.target_position = WALKING_target_node
+		
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "player":
+	if body.name in target_names && current_state != State.PURSUING:
 		print("Player Entered")
-		player_in_sight = true
-		player_ray_cast_check = false
+		alerted = true
 		player_body = body
 		
 		
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.name == "player":
+	if body.name in target_names && body.name == player_body.name:
 		print("Player Exited")
+		alerted = false
 		player_in_sight = false
-		player_ray_cast_check = false
-		player_body = null
+		if default_state == State.WALKING:
+			init_walking()
 
 
 func _on_fire_timer_timeout() -> void:
-	if player_in_sight && player_ray_cast_check:
-		#global_position.angle_to(player_body.global_position)
+	if current_state == State.PURSUING:
 		bulletfire.fire_bullet(global_rotation, 150)
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+	move_and_slide()
